0dc4440e1fe78fa7f903b4f37adae500cbdefc1b
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-05-06 19:17:45 +0700

    Update 07_sum_number.c

--- 07_sum_number.c
+++ 07_sum_number.c
@@ -3,17 +3,17 @@
 int main()
 {
     // หาผลรวม 123456 = 1+2+3+4+5+6 = 21
-    double x;
-    scanf("%f",&x);
-    double sum = 0;
+    int x;
+    scanf("%d",&x);
+    int sum = 0;
 
     do{
-        
-        
+        sum += x%10;      
+        x = x/10;
         
     }while(x>0);
 
-    printf("%.0f",sum);
+    printf("%d",sum);
 
     return 0;
 }
