09098baee541454c3d8945f86ab2cafb9dea5d93
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-05-06 19:29:58 +0700

    Create if01_Even_Odd_number.cpp

---
+++ if01_Even_Odd_number.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int a;
+
+    cin>>a;
+    if (a%2 == 0){
+        cout<<"even number";
+    }else{
+        cout<<"odd number";
+    }
+
+    return 0;
+}
